3f17105e510a3c0fb8a853fdddaf49ca9cc8b46d
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2020-04-22 05:42:49 -0500

    Task_3: Task_3_1: API commands GET/POST Methods Added Task_3_2: Serealization/Deserealization Added Features: - New User is Created using API Only - Updating Added User Using Selenium and Cucumber

--- src/test/java/stepDefs/MyStepdefs.java
+++ src/test/java/stepDefs/MyStepdefs.java
@@ -9,16 +9,21 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import pageObjects.CommandsAPI;
+import pageObjects.UpdateUserPage;
 import plainOldJavaObjects.User;
 
 public class MyStepdefs {
 
     private WebDriver driver;
     private CommandsAPI cmd_API;
+    private UpdateUserPage objUserPage;
     private User myUser;
+    private int userId;
 
     final String DRIVER_PATH = "/Users/viktormacbook/Documents/Selenium/chromedriver";
     final String USERS_ENDPOINT = "https://selenium-spring2020.000webhostapp.com/api/v1/users";
@@ -28,6 +33,7 @@ public class MyStepdefs {
     public void initialiseMainElements(){
         System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
         driver = new ChromeDriver();
+        objUserPage = new UpdateUserPage(driver);
         cmd_API = new CommandsAPI();
         myUser = new User("name99", "username99", "+3423456432", "name99@gmail.com", "9999 W Michigan St");
     }
@@ -40,22 +46,30 @@ public class MyStepdefs {
     @Given("^New user is created via API$")
     public void newUserIsCreatedViaAPI() throws UnirestException {
         HttpResponse<JsonNode> response = cmd_API.post_API(USERS_ENDPOINT,myUser);
-        System.out.println(response.getBody().toString());
+        JSONObject userJsonObject = response.getBody().getObject();
+        userId = (int)(userJsonObject.get("id"));
     }
 
     @When("^User navigates to page to update the created user$")
-    public void userNavigatesToPageToUpdateTheCreatedUser() {
+    public void userNavigatesToPageToUpdateTheCreatedUser() throws InterruptedException {
+        driver.get(GET_USER_ENDPOINT + userId);
     }
 
     @And("^User types new personal data of choosed user$")
-    public void userTypesNewPersonalDataOfChoosedUser() {
-    }
-
-    @And("^User clicks on update button$")
-    public void userClicksOnUpdateButton() {
+    public void userTypesNewPersonalDataOfChoosedUser() throws InterruptedException {
+        myUser = new User("updatedName", "updatedUsername", "updatedPhone", "updatedEmail", "updatedAddress");
+        objUserPage.updateUser(myUser);
     }
 
     @Then("^User checks if data is correct$")
     public void userChecksIfDataIsCorrect() {
+        System.out.println(objUserPage.getId() + " " + objUserPage.getName());
+        Assert.assertEquals(objUserPage.getId(), Integer.toString(userId));
+        Assert.assertEquals(objUserPage.getName(), myUser.name);
+        Assert.assertEquals(objUserPage.getUsername(), myUser.username);
+        Assert.assertEquals(objUserPage.getEmail(), myUser.email);
+        Assert.assertEquals(objUserPage.getPhone(), myUser.phone);
+        Assert.assertEquals(objUserPage.getAddress(), myUser.address);
+        objUserPage.clickUpdate();
     }
 }
